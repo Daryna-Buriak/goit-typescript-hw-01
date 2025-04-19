760d1d540b43e1789380c29c5edcbe99f7978c2a
Daryna <buriak.daryna@gmail.com>
2025-04-19 17:15:13 +0300

    Add basic TS

---
+++ src/basic/5.ts
@@ -0,0 +1,11 @@
+const enum DayOfWeek {
+  Monday = "false",
+  Tuesday = "false",
+  Wednesday = "false",
+  Thursday = "false",
+  Friday = "false",
+  Saturday = "true",
+  Sunday = "true",
+}
+
+let isWeekend: (day: string) => boolean;
